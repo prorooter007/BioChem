ed46382595888d015cae7aa9b21282f9dc89a413
Shreyash Wasnik <shreyashwasnik112@gmail.com>
2023-03-08 15:26:53 +0530

    UI: update first two pages
    Import Sanskruti's first two pages
    
    Signed-off-by: Shreyash Wasnik <shreyashwasnik112@gmail.com>

--- app/src/main/java/com/example/biochem/nextpg2.kt
+++ app/src/main/java/com/example/biochem/nextpg2.kt
@@ -5,18 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
-
+class nextpg2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_nextpg2)
 
         val buttonMainActivit: Button = findViewById(
-            R.id.button5
+            R.id.button7
         )
         buttonMainActivit.setOnClickListener {
             val mainActivityIntent = Intent(
-                applicationContext, nextpg2::class.java
+                applicationContext, TestName::class.java
             )
             startActivity(mainActivityIntent)
         }

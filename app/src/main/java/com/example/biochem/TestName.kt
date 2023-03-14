eeed543d0d417daaca2e87a23746772605473ba2
Shreyash Wasnik <shreyashwasnik112@gmail.com>
2023-03-14 18:05:34 +0530

    activity:TestName: open camera from standard and reagent blank button
    Signed-off-by: Shreyash Wasnik <shreyashwasnik112@gmail.com>

--- app/src/main/java/com/example/biochem/TestName.kt
+++ app/src/main/java/com/example/biochem/TestName.kt
@@ -1,13 +1,22 @@
 package com.example.biochem
 
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
 class TestName : AppCompatActivity() {
+
+    lateinit var imageView: ImageView
+    lateinit var button: Button
+    val REQUEST_IMAGE_CAPTURE=100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_name)
@@ -22,6 +31,44 @@ class TestName : AppCompatActivity() {
                 applicationContext, TestName2::class.java
             )
             startActivity(secondActivityIntent)
+        }
+
+
+        imageView = findViewById(R.id.imageView6)
+        button = findViewById(R.id.button7)
+
+        button.setOnClickListener{
+            val takePictureIntent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            try{
+                startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE)
+            }catch(e:ActivityNotFoundException){
+                Toast.makeText(this,"Error: "+e.localizedMessage,Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    imageView = findViewById(R.id.imageView6)
+    button = findViewById(R.id.button8)
+
+    button.setOnClickListener{
+        val takePictureIntent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        try{
+            startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE)
+        }catch(e:ActivityNotFoundException){
+            Toast.makeText(this,"Error: "+e.localizedMessage,Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+    override fun onActivityResult(requestCode:Int, resultCode: Int, data: Intent?) {
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
+        }
+        else{
+            super.onActivityResult(requestCode,resultCode, data)
+
         }
     }
 }

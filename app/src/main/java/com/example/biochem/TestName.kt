b657ee428e173212cc3241d732bb73de369d105f
Shreyash Wasnik <shreyashwasnik112@gmail.com>
2023-03-29 08:53:57 +0530

    main: code cleanup and fix camera permission
    Signed-off-by: Shreyash Wasnik <shreyashwasnik112@gmail.com>

--- app/src/main/java/com/example/biochem/TestName.kt
+++ app/src/main/java/com/example/biochem/TestName.kt
@@ -3,6 +3,7 @@ package com.example.biochem
 
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
@@ -11,13 +12,15 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 
 class TestName : AppCompatActivity() {
 
     lateinit var imageView: ImageView
     lateinit var button: Button
-    val REQUEST_IMAGE_CAPTURE=100
+    val REQUEST_IMAGE_CAPTURE = 1222
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,46 +28,8 @@ class TestName : AppCompatActivity() {
         setContentView(R.layout.activity_test_name)
 
         var text = intent.getStringExtra("test")
-        val  textView_4 = findViewById<TextView>(R.id.textView4)
+        val textView_4 = findViewById<TextView>(R.id.textView4)
         textView_4.setText(text)
-
-//
-//        var text = intent.getStringExtra("key_1")
-//
-//        if (text != null){
-//            textView_4.setText(text)
-//        }
-//
-//        var text_2 = intent.getStringExtra("key_2")
-//
-//        if (text_2 != null){
-//            textView_4.setText(text_2)
-//        }
-//
-//        var text_3 = intent.getStringExtra("key_3")
-//
-//        if (text_3 != null){
-//            textView_4.setText(text_3)
-//        }
-//
-//        var text_4 = intent.getStringExtra("key_4")
-//
-//        if (text_4 != null){
-//            textView_4.setText(text_4)
-//        }
-//
-//        var text_5 = intent.getStringExtra("key_5")
-//
-//        if (text_5 != null){
-//            textView_4.setText(text_5)
-//        }
-//
-//        var text_6 = intent.getStringExtra("key_6")
-//
-//        if (text_6 != null){
-//            textView_4.setText(text_6)
-//        }
-
 
         val buttonSecondActivity: Button = findViewById(
             R.id.button6
@@ -77,42 +42,41 @@ class TestName : AppCompatActivity() {
             startActivity(secondActivityIntent)
         }
 
-
         imageView = findViewById(R.id.imageView6)
         button = findViewById(R.id.button7)
 
-        button.setOnClickListener{
-            val takePictureIntent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-
-            try{
-                startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE)
-            }catch(e:ActivityNotFoundException){
-                Toast.makeText(this,"Error: "+e.localizedMessage,Toast.LENGTH_SHORT).show()
-            }
+        /**set camera Open*/
+        button.setOnClickListener {
+            val cameraInt = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraInt, REQUEST_IMAGE_CAPTURE)
         }
 
-    imageView = findViewById(R.id.imageView6)
-    button = findViewById(R.id.button8)
+        imageView = findViewById(R.id.imageView6)
+        button = findViewById(R.id.button8)
 
-    button.setOnClickListener{
-        val takePictureIntent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-
-        try{
-            startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE)
-        }catch(e:ActivityNotFoundException){
-            Toast.makeText(this,"Error: "+e.localizedMessage,Toast.LENGTH_SHORT).show()
-        }
-    }
-}
-
-    override fun onActivityResult(requestCode:Int, resultCode: Int, data: Intent?) {
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            imageView.setImageBitmap(imageBitmap)
-        }
-        else{
-            super.onActivityResult(requestCode,resultCode, data)
-
+        /**get Permission*/
+        if (ContextCompat.checkSelfPermission(
+                applicationContext, android.Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_DENIED
+        )
+            ActivityCompat.requestPermissions(
+                this, arrayOf(android.Manifest.permission.CAMERA),
+                REQUEST_IMAGE_CAPTURE
+            )
+        /**set camera Open*/
+        button.setOnClickListener {
+            val cameraInt = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraInt, REQUEST_IMAGE_CAPTURE)
         }
     }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            /**save to Image In layout*/
+            val images: Bitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(images)
+        }
+    }
+    /** ok now run it*/
+
 }

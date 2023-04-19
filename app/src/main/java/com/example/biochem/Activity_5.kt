da48e48eeaf3378fe11f087a24e64ff840578da0
Shreyash Wasnik <shreyashwasnik112@gmail.com>
2023-04-20 00:45:03 +0530

    main: Activity_5: kt: jump to the previous activity after pressing the "back" button
    Signed-off-by: Shreyash Wasnik <shreyashwasnik112@gmail.com>

--- app/src/main/java/com/example/biochem/Activity_5.kt
+++ app/src/main/java/com/example/biochem/Activity_5.kt
@@ -1,13 +1,25 @@
 package com.example.biochem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Activity_5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_5)
 
+        // Set up the "Back" button
+        val backButton = findViewById<Button>(R.id.button_back)
+        backButton.setOnClickListener {
+            // Add code to go to the previous activity
+            val secondActivityIntent = Intent(
+                applicationContext, Activity_4::class.java
+            )
+
+            startActivity(secondActivityIntent)
+        }
 
     }
 }
